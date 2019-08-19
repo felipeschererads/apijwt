05c17b07ee6d9d82620895c80f621c69acbcf808
“Felipe <felipescherer.ads@gmail.com>
2019-08-18 21:35:21 -0300

    implementação jwt

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const User = require('../models/user')
 const router = express.Router();
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const authConfig = require('../config/auth')
 
+function genereteToken(params = {}) {
+  return jwt.sign({ params }, authConfig.secret, {
+    /**Milesegundos */
+    expiresIn: 86400
+  })
+
+}
+/*
+*Rota de registro do usuario
+*/
 router.post('/register', async (req, res) => {
 
   const { email } = req.body;
+  
   try {
     if (await User.findOne({ email }))
       return res.status(400).send({ error: 'Este usuário já existe.' })
@@ -12,11 +26,35 @@ router.post('/register', async (req, res) => {
     const user = await User.create(req.body)
     user.password = undefined;
 
-    return res.send({ user })
+    return res.send({
+      user,
+      token: genereteToken({ id: user.id })
+    })
 
   } catch (error) {
     return res.status(400).send({ error: 'Registro falhou' })
   }
+
+})
+
+router.post('/authenticate', async (req, res) => {
+
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select('+password')
+
+  if (!user)
+    return res.status(400).send({ error: 'Usuário não encontrado' })
+
+  if (!await bcrypt.compare(password, user.password))
+    return res.status(400).send({ error: 'Senha incorreta.' })
+
+  user.password = undefined;
+
+  return res.send({
+    user,
+    token: genereteToken({ id: user.id })
+  })
 
 })
 
